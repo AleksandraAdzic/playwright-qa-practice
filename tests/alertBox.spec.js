b5ce3c3e53408281a66ce8078fc912c76278dcbc
Aleksandra Adzic <aleksandra93kv@yahoo.com>
2024-04-22 11:22:47 +0200

    Changing page.on

--- tests/alertBox.spec.js
+++ tests/alertBox.spec.js
@@ -7,14 +7,14 @@ test.beforeEach(async ({ page }) => {
   await basePage.toGo("https://www.qa-practice.com/elements/alert/alert");
 });
 
-test.only("AlertBox - confirm dialog", async ({ page }) => {
+test("AlertBox - confirm dialog", async ({ page }) => {
   const poManager = new POManager(page);
   const alertBox = poManager.getAlertBoxPage();
-
-  await alertBox.clickClickButton();
   page.on("dialog", async (dialog) => {
     expect(dialog.type()).toContain("confirm");
     expect(dialog.message()).toContain("I am an alert!");
     await dialog.accept();
   });
+
+  await alertBox.clickClickButton();
 });
